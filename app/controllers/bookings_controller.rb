ca5c9a723b2542a4974fad80e5cb1fe72602f65b
Jon Entwistle <jonny3107@aol.com>
2022-11-22 15:40:53 +0000

    crud complete

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,39 @@
+class BookingsController < ApplicationController
+  before_action :set_chef, only: [:new, :create]
+  before_action :set_user, only: [:new, :create, :destroy]
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.chef = @chef
+    @booking.user = @user
+      if @booking.save!
+        redirect_to user_path(@user)
+      else
+        render :new, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to user_path(@user), status: :see_other
+  end
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:description)
+end
+
+def set_chef
+  @chef = Chef.find(params[:chef_id])
+end
+
+def set_user
+  @user = current_user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :chefs
+  resources :chefs do
+    resources :bookings, only: [:index, :show, :new, :create, :delete]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
