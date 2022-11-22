af7d6555cbec230090365a49a7486d8dfa0eb61f
Jon Entwistle <jonny3107@aol.com>
2022-11-22 17:22:26 +0000

    some user and chef data entered into seed file and hashed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Chef.destroy_all
+# User.destroy_all
+# Booking.destroy_all
+
+# User.create([{first_name: "Gordon", last_name: "Ramsay", email: "gordon@test.com", password: "password" }, {first_name: "Michel", last_name: "Roux Jr", email: "michel@test.com", password: "password" }, {first_name: "Angela", last_name: "Hartnett", email: "angela@test.com", password: "password" }, {first_name: "Gino", last_name: "D'Acampo", email: "gino@test.com", password: "password" }, {first_name: "Atul", last_name: "Kocchar", email: "atul@test.com", password: "password" }])
+
+# Chef.create([{user_id: 1, details: "Gordon James Ramsay OBE is a British chef, restaurateur, television personality and writer. His restaurant group, Gordon Ramsay Restaurants, was founded in 1997 and has been awarded 17 Michelin stars overall; it currently holds a total of seven.", cuisine: "British" }, {user_id: 2, details: "Michel Albert Roux previously known as Michel Roux Jr., is an English-French two Michelin starred chef. He owns the London restaurant Le Gavroche.", cuisine: "Classic French" }, {user_id: 3, details: "Angela Maria Hartnett OBE is an English Michelin-starred chef. A protégée of Gordon Ramsay who became well known by her appearances on British television, she was Chef-Patron at Angela Hartnett at The Connaught in London.", cuisine: "Italian" }, {user_id: 4, details: "Gennaro Sheffield D'Acampo is an Italian celebrity chef and media personality based in the United Kingdom, best known for his food-focused television shows and cookbooks. D'Acampo rose to fame as a regular chef on the ITV show This Morning.", cuisine: "Italian" }, {user_id: 5, details: "Atul Kochhar is an Indian-born, British-based chef and television personality. Kochhar was one of the first two Indian chefs to receive a Michelin star, awarded in London in 2001 whilst at Tamarind. He opened his own restaurant Benares, which won him a second Michelin star in 2007.", cuisine: "Indian" }])
