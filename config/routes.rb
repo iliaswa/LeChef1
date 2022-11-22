d98b2ffdca530793f96388f0dcb665d92d407c6d
Oona Singh <oonarsingh@gmail.com>
2022-11-22 11:25:18 +0000

    chef changes for george

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :chefs
   # Defines the root path route ("/")
   # root "articles#index"
 end
