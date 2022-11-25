64f2c274ad0de4e59e0c4da1b5f703719899f228
Ilias Wardere <iwardere1@gmail.com>
2022-11-25 13:04:05 +0000

    fixed bugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  include PgSearch::Model
-  multisearchable against: [:cuisine, :details]
+  #include PgSearch::Model
+  #multisearchable against: [:cuisine, :details]
 end
